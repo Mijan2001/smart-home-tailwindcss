93b39edf1c8566215ac4cd747f4d834862e69f64
Mijan2001 <mijan.cse19@gmail.com>
2024-08-19 16:55:14 +0600

    html,tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    darkMode: 'class', // or 'media' if you want to use system preferences
+    theme: {
+        extend: {}
+    },
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {}
+    }
+};
